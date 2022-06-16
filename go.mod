d91ad6d5ba758fad32dc3ed634ebfb81082c9906
snoworly <snoworly@pm.me>
2022-06-16 13:48:22 +0900

    add tool to init on-chain faucet

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/magicmarkets/solana-go-tools
 go 1.17
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 	github.com/gagliardetto/binary v0.6.1
+	github.com/gagliardetto/gofuzz v1.2.2
 	github.com/gagliardetto/solana-go v1.4.0
+	github.com/gagliardetto/treeout v0.1.4
+	github.com/stretchr/testify v1.6.1
 )
 
 require (
@@ -14,10 +18,8 @@ require (
 	github.com/aybabtme/rgbterm v0.0.0-20170906152045-cc83f3b3ce59 // indirect
 	github.com/blendle/zapdriver v1.3.1 // indirect
 	github.com/buger/jsonparser v1.1.1 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dfuse-io/logging v0.0.0-20210109005628-b97a57253f70 // indirect
 	github.com/fatih/color v1.9.0 // indirect
-	github.com/gagliardetto/treeout v0.1.4 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/gorilla/rpc v1.2.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
@@ -31,6 +33,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mostynb/zstdpool-freelist v0.0.0-20201229113212-927304c0c3b1 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/teris-io/shortid v0.0.0-20201117134242-e59966efd125 // indirect
 	github.com/tidwall/gjson v1.9.3 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
@@ -43,4 +46,5 @@ require (
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
---
+++ init_escrow_faucet/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/rpc"
+	"github.com/gagliardetto/solana-go/rpc/ws"
+	sgo "github.com/magicmarkets/solana-go-tools"
+	"github.com/magicmarkets/solana-go-tools/generated/escrow_token_mint"
+)
+
+func main() {
+	ctx := context.Background()
+
+	var authority, mintFile, receiverAddress, clusterRPC, clusterWS string
+
+	flag.StringVar(&authority, "authority", "", "payer private key from solana-keygen file that becomes the sweep authority")
+	flag.StringVar(&mintFile, "mint", "", "mint key from solana-keygen file")
+	flag.StringVar(&clusterRPC, "url", rpc.LocalNet_RPC, "solana cluster rpc url")
+	flag.StringVar(&clusterWS, "ws", rpc.LocalNet_WS, "solana cluster websocket url")
+	flag.Parse()
+
+	payer, err := solana.PrivateKeyFromSolanaKeygenFile(authority)
+	if err != nil {
+		fmt.Println("solana.PrivateKeyFromSolanaKeygenFile failed:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("payer pubkey:", payer.PublicKey())
+
+	if receiverAddress == "" {
+		receiverAddress = payer.PublicKey().String()
+	}
+
+	rpcURL, err := sgo.RPCFromMoniker(clusterRPC)
+	if err == nil {
+		clusterRPC = rpcURL
+	}
+
+	wsURL, err := sgo.WSFromMoniker(clusterWS)
+	if err == nil {
+		clusterWS = wsURL
+	}
+
+	rpcClient := rpc.New(clusterRPC)
+
+	wsClient, err := ws.Connect(context.Background(), clusterWS)
+	if err != nil {
+		fmt.Println("ws.Connect failed:", err)
+		os.Exit(1)
+	}
+
+	escrowMint, err := solana.PrivateKeyFromSolanaKeygenFile(mintFile)
+	if err != nil {
+		fmt.Println("solana.PrivateKeyFromSolanaKeygenFile failed:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("mint pubkey: ", escrowMint.PublicKey())
+
+	faucet, _, err := solana.FindProgramAddress([][]byte{payer.PublicKey().Bytes(), []byte("faucet_vault")}, escrow_token_mint.ProgramID)
+	if err != nil {
+		fmt.Println("solana.FindProgramAddress failed:", err)
+		os.Exit(1)
+	}
+
+	builder := escrow_token_mint.NewInitializeInstruction(
+		payer.PublicKey(),
+		escrowMint.PublicKey(),
+		faucet,
+		solana.SystemProgramID,
+		solana.SysVarRentPubkey,
+		solana.TokenProgramID,
+	)
+
+	inst, err := builder.ValidateAndBuild()
+	if err != nil {
+		fmt.Println("escrow_token_mint.NewInitializeInstruction failed:", err)
+		os.Exit(1)
+	}
+
+	sig, err := sgo.SendTx(ctx, rpcClient, wsClient, []solana.Instruction{inst}, []solana.PrivateKey{}, payer, true)
+	if err != nil {
+		fmt.Println("sgo.SendTx failed:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("faucet created:", faucet.String())
+	fmt.Println(sig)
+}
